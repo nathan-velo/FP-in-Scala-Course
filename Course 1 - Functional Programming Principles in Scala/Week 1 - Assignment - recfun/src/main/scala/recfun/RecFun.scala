927f2c632aa7e10960839b812ba0159f8a794bb1
nathan-velo <59030876+nathan-velo@users.noreply.github.com>
2024-06-20 21:34:06 +0000

    Formatted RecFun code

--- Course 1 - Functional Programming Principles in Scala/Week 1 - Assignment - recfun/src/main/scala/recfun/RecFun.scala
+++ Course 1 - Functional Programming Principles in Scala/Week 1 - Assignment - recfun/src/main/scala/recfun/RecFun.scala
@@ -14,8 +14,7 @@ object RecFun extends RecFunInterface:
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int =
-    if c == 0 || c == r then 1 else
-      pascal(c-1,r-1) + pascal(c, r-1)
+    if c == 0 || c == r then 1 else pascal(c-1,r-1) + pascal(c, r-1)
   /**
    * Exercise 2
    */
@@ -23,10 +22,9 @@ object RecFun extends RecFunInterface:
     def balance_helper(chars: List[Char], open: Int): Boolean =
       if chars.isEmpty then open == 0
       else if open < 0 then false
-      else
-        if chars.head == '(' then balance_helper(chars.tail,open + 1) 
-        else if chars.head == ')' then balance_helper(chars.tail,open - 1)
-        else balance_helper(chars.tail, open)
+      else if chars.head == '(' then balance_helper(chars.tail,open + 1)
+      else if chars.head == ')' then balance_helper(chars.tail,open - 1)
+      else balance_helper(chars.tail, open)
 
     balance_helper(chars, 0)
 
